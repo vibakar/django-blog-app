d4ded2cbb854df840c05c164e0094b4f9ffd18cc
vibakar <vibakar23@gmail.com>
2024-07-05 08:48:27 +0100

    Using db for storing data

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from blog import models
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['author', 'title', 'date']
+    list_filter = ['author', 'tags', 'date']
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(models.Post, PostAdmin)
+admin.site.register(models.Author)
+admin.site.register(models.Tag)
--- app/blog/models.py
+++ app/blog/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.caption}"
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=250)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinLengthValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.CASCADE,
+                               related_name="posts")
+    tags = models.ManyToManyField(Tag, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.title}"
